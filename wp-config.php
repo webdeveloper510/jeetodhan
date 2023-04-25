890b195539dd3dd2bd58edc64c9a266641fbbcd6
webdeveloper510 <developerweb6@gmail.com>
2023-04-25 15:28:25 +0530

    Changes

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', ' jeetodhan' );
+define( 'DB_NAME', 'jeetodhan' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
@@ -87,7 +87,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
